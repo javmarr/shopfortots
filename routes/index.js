e152db2eafbb138e74f348e49d572dfe7c24a9fe
Samantha <samindalecio@gmail.com>
2016-07-27 16:39:25 -0500

    you can now use an array to fill the table and the price is refrenced through the checkbox

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shop for Tots' });
+  res.render('index', { title: 'Shop for Tots', items:[{
+  id: "10",
+  title: 'toy1' ,
+  price: 1,
+  image: 'stuff'
+  },{
+  id: "20",
+  title: 'toy2' ,
+  price: 3,
+  image: 'stuffy'
+  }]
+  })
+
 });
+
 
 module.exports = router;
